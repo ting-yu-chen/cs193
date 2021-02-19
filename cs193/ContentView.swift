b976200258bf8681aa2c91dd0bc4bae58ca3d446
tinchen <tinchen@student.ethz.ch>
2021-02-19 11:22:20 +0100

    Initial Commit

---
+++ cs193/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cs193
+//
+//  Created by TingyuChen on 2021/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs193/cs193App.swift
@@ -0,0 +1,17 @@
+//
+//  cs193App.swift
+//  cs193
+//
+//  Created by TingyuChen on 2021/2/19.
+//
+
+import SwiftUI
+
+@main
+struct cs193App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
