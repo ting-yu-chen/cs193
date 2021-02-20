538f5c2b8f6bc074e84d0122e81b67b5143e600a
tinchen <tinchen@student.ethz.ch>
2021-02-20 12:05:30 +0100

    week1:add cardviews

--- cs193/ContentView.swift
+++ cs193/ContentView.swift
@@ -9,11 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            ForEach(0..<4){
+                index in
+                if index%2 == 0{
+                    CardView(isFaceUp: true)
+                }
+                else {
+                    CardView(isFaceUp: false)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        
     }
 }
 
+struct CardView: View{
+    var isFaceUp:Bool;
+    var body: some View{
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 15.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 15.0).stroke()
+                Text("👻").font(.largeTitle)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 15.0).fill()
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
